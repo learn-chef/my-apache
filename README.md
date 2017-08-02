f321d821ce6d5abd82bba91fb7ee873d7c71b308
tpetchel <tpetchel@gmail.com>
2017-08-02 08:49:44 -0400

    Initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# my-apache
+
+This InSpec profile is for the Learn Chef module [Create a custom InSpec profile](https://learn.chef.io/modules/create-a-custom-profile/).
---
+++ controls/apache.rb
@@ -0,0 +1,8 @@
+require_controls 'apache-baseline' do
+  control 'apache-01'
+  control 'apache-04'
+  control 'apache-05'
+  control 'apache-06'
+
+  control 'apache-14'
+end
